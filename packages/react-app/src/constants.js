a7befcc0b6e3185a6e2dd48975f849d6f975f086
Ben Rasmussen <bigbadbeard@tutanota.com>
2023-06-10 14:50:24 -0500

    challenge 0 done

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "b7746dc195604917ab35c524869e3f03";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
@@ -18,7 +18,8 @@ export const NETWORKS = {
   mainnet: {
     name: "mainnet",
     color: "#ff8b9e",
-    chainId: 1,
+    chainId: 1
+    ,
     rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
     blockExplorer: "https://etherscan.io/",
   },
